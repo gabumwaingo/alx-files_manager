b011ade1c2f05f40bc3b7d75d94c5570f59f1096
gabumwaingo <gabumwaingo@gmail.com>
2023-12-08 17:38:56 -0800

    Task 1

---
+++ utils/db.js
@@ -0,0 +1,38 @@
+const { MongoClient } = require('mongodb');
+
+const DB_PORT = process.env.DB_PORT || 27017;
+const DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
+const url = 'mongodb://{DB_HOST}:{DB_PORT}';
+
+
+class DBClient {
+	constructor() {
+		MongoClient.connect(url, {useUnifiedTopology: true}, (err, client) => {
+			if(!err) {
+				this.db = client.db(DB_DATABASE);
+				this.users = this.db.collection('users');
+				this.files = this.db.collection('files');
+			} else {
+				console.log(err.message);
+				this.db = false;
+			}
+		});
+	}
+
+	isAlive() {
+		return !!this.db;
+	}
+
+	async nbUsers() {
+		return this.users.countDocuments();
+	}
+	async nbFiles() {
+		return this.files.countDocuments();
+	}
+	async getUsers(query) {
+		const user = await this.db.collection('users').findOne(query);
+	}
+}
+
+const dbClient = new DBClient();
+module.exports = dbClient;
